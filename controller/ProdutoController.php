9909dc0f329eb74bdc4448d4d9e2b70e10e48d12
hssuper <higorsouza2306@gmail.com>
2021-07-05 11:53:59 -0300

    up pra geral

---
+++ controller/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'C:/xampp/htdocs/PHPMatutino01/dao/daoProduto.php';
+include_once 'C:/xampp/htdocs/PHPMatutino01/model/Produto.php';
+class ProdutoController {
+    public function inserirproduto($nomeProduto,$vlrCompra,$vlrVenda,$qtdEstoque) {
+       $produto = new Produto();
+       $produto->setNomeProduto($nomeProduto);
+       $produto->setVlrCompra($vlrCompra);
+       $produto->setVlrVenda($vlrVenda);
+       $produto->setQtdEstoque($qtdEstoque);
+       
+       $daoProduto = new daoProduto();
+       return $daoProduto->inserir($produto);
+    }
+}
