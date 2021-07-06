446a0420a5d856120d3305b3b1b43a6bf6e1dee8
hssuper <higorsouza2306@gmail.com>
2021-07-06 09:29:44 -0300

    gg

---
+++ dao/daoLivro.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'C:/xampp/htdocs/PHPMatutino01/bd/Conecta.php';
+include_once 'C:/xampp/htdocs/PHPMatutino01/model/Produto.php';
+class daoProduto {
+    public function inserir(Produto $produto){
+        $conn = new Conecta();
+        if($conn->conectadb()){
+            $nomeProduto = $produto->getNomeProduto();
+            $vlrCompra = $produto->getVlrCompra();
+            $vlrVenda = $produto->getVlrVenda();
+            $c = $produto->getQtdEstoque();
+            $sql = "insert into produto values(null,'$nomeProduto','$vlrCompra ','$vlrVenda','$vlrVenda')";
+            if(mysqli_query($conn->conectadb(), $sql)){
+                $msg = "<p style='color:green;'>".
+                        "Dados cadastrados com sucesso</p>";
+            }
+        }else{
+            $msg = "<p style='color:red;'>".
+                        "Erro na conexão com o banco de dados</p>";
+        }
+        mysqli_close($conn->conectadb());
+        
+      
+    }
+}
