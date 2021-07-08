22e08d87d29aab3b4def5bbdaac449479567f0ce
hssuper <higorsouza2306@gmail.com>
2021-07-08 09:00:10 -0300

    up

--- cadastroP.php
+++ cadastroP.php
@@ -53,9 +53,8 @@ include_once 'controller/ProdutoController.php';
             <div class="row" style="margin-top: 30px;">
                 <div class="col-8 offset-2">
 
-                    <div class="card-header bg-light text-center border"
-                         style="padding-bottom: 15px; padding-top: 15px;">
-                        Cadastro de Produto
+                    <div class="card-header bg-dark text-center border text-white">
+                        <strong>Cadastro de Produto</strong>
                     </div>
                     <div class="card-body border">
                         <?php
@@ -73,7 +72,7 @@ include_once 'controller/ProdutoController.php';
                         ?>
                         <form method="post" action="">
                             <div class="row">
-                                <div class="col-md-6 offset-md-3">
+                                <div class="col-md-8 offset-md-3">
                                     <label>Código: </label> <br> 
                                     <label>Produto</label>  
                                     <input class="form-control" type="text" 
@@ -100,65 +99,68 @@ include_once 'controller/ProdutoController.php';
         </div>
         <div class="row" style="margin-top: 30px;">
             <table class="table table-striped table-responsive">
-                <thead class="table-dark">
-                    <tr><th>Código</th>
-                        <th>Nome</th>
-                        <th>Compra (R$)</th>
-                        <th>Venda (R$)</th>
-                        <th>Estoque</th>
-                        <th>Ações</th></tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $pcTable = new ProdutoController();
-                    $listaProdutos = $pcTable->listarProdutos();
-                    $a = 0;
-                    if ($listaProdutos != null) {
-                        foreach ($listaProdutos as $lp) {
-                            $a++;
-                            ?>
-                            <tr>
-                                <td><?php print_r($lp->getIdProduto()); ?></td>
-                                <td><?php print_r($lp->getNomeProduto()); ?></td>
-                                <td><?php print_r($lp->getVlrCompra()); ?></td>
-                                <td><?php print_r($lp->getVlrVenda()); ?></td>
-                                <td><?php print_r($lp->getQtdEstoque()); ?></td>
-                                <td><a class="btn btn-light" 
-                                       href="#?id=<?php echo $lp->getIdProduto(); ?>">
-                                        <img src="img/edita.png" width="32"></a>
-                                    <button type="button" 
-                                            class="btn btn-light" data-bs-toggle="modal" 
-                                            data-bs-target="#exampleModal<?php echo $a;?>">
-                                        <img src="img/delete.png" width="32"></button></td>
-                            </tr>
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal<?php echo $a;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">deseja excluir</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Contexto....<?php echo $lp->getIdProduto(); ?>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-primary">Sim</button>
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não</button>
+                <div class="row" style="margin-top: 30px;">
+                    <thead class="table-dark"
+                           >
+                        <tr><th>Código</th>
+                            <th>Nome</th>
+                            <th>Compra (R$)</th>
+                            <th>Venda (R$)</th>
+                            <th>Estoque</th>
+                            <th>Ações</th></tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $pcTable = new ProdutoController();
+                        $listaProdutos = $pcTable->listarProdutos();
+                        $a = 0;
+                        if ($listaProdutos != null) {
+                            foreach ($listaProdutos as $lp) {
+                                $a++;
+                                ?>
+                                <tr>
+                                    <td><?php print_r($lp->getIdProduto()); ?></td>
+                                    <td><?php print_r($lp->getNomeProduto()); ?></td>
+                                    <td><?php print_r($lp->getVlrCompra()); ?></td>
+                                    <td><?php print_r($lp->getVlrVenda()); ?></td>
+                                    <td><?php print_r($lp->getQtdEstoque()); ?></td>
+                                    <td><a class="btn btn-light" 
+                                           href="#?id=<?php echo $lp->getIdProduto(); ?>">
+                                            <img src="img/edita.png" width="32"></a>
+                                        <button type="button" 
+                                                class="btn btn-light" data-bs-toggle="modal" 
+                                                data-bs-target="#exampleModal<?php echo $a; ?>">
+                                            <img src="img/delete.png" width="32"></button></td>
+                                </tr>
+                                <!-- Modal -->
+                            <div class="modal fade" id="exampleModal<?php echo $a; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">deseja excluir</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Contexto....<?php echo $lp->getIdProduto(); ?>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary">Sim</button>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                        <?php
+                            <?php
+                        }
                     }
-                }
-                ?>
-                </tbody>
+                    ?>
+                    </tbody>
             </table>
-        </div>     
-    </div>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+        </div>
+    </div>     
+</div>
+<script src="js/bootstrap.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
 
