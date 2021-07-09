e8e9922e33d86a64dd7c71b3d02774ae57b8bb2a
hssuper <higorsouza2306@gmail.com>
2021-07-09 08:15:54 -0300

    a

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -77,7 +77,7 @@ $pr = new Produto();
                                     URL='cadastroProduto.php'\">";
                             }
                         }
-                        if(isset($_POST['limpar'])){
+                        if (isset($_POST['limpar'])) {
                             $pc2 = new ProdutoController();
                             $pr = $pc2->limpar();
                             unset($_POST['limpar']);
@@ -97,16 +97,16 @@ $pr = new Produto();
                                     <label>Código: </label> <br> 
                                     <label>Produto</label>  
                                     <input class="form-control" type="text" 
-                                           name="nomeProduto" value="<?php echo $pr->getNomeProduto();?>">
+                                           name="nomeProduto" value="<?php echo $pr->getNomeProduto(); ?>">
                                     <label>Valor de Compra</label>  
                                     <input class="form-control" type="text" 
-                                           value="<?php echo $pr->getVlrCompra();?>" name="vlrCompra">  
+                                           value="<?php echo $pr->getVlrCompra(); ?>" name="vlrCompra">  
                                     <label>Valor de Venda</label>  
                                     <input class="form-control" type="text" 
-                                           value="<?php echo $pr->getVlrVenda();?>" name="vlrVenda"> 
+                                           value="<?php echo $pr->getVlrVenda(); ?>" name="vlrVenda"> 
                                     <label>Qtde em Estoque</label>  
                                     <input class="form-control" type="number" 
-                                           value="<?php echo $pr->getQtdEstoque();?>" name="qtdEstoque">
+                                           value="<?php echo $pr->getQtdEstoque(); ?>" name="qtdEstoque">
                                     <input type="submit" name="cadastrarProduto"
                                            class="btn btn-success btInput" value="Enviar">
                                     &nbsp;&nbsp;
@@ -120,71 +120,71 @@ $pr = new Produto();
             <div class="col-md-8">
                 <div class="row">
                     <div class="col-md-12">
-                    <table class="table table-striped table-responsive"
-                           style="border-radius: 3px; overflow:hidden;">
-                        <thead class="table-dark">
-                            <tr><th>Código</th>
-                                <th>Nome</th>
-                                <th>Compra (R$)</th>
-                                <th>Venda (R$)</th>
-                                <th>Estoque</th>
-                                <th>Ações</th></tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $pcTable = new ProdutoController();
-                            $listaProdutos = $pcTable->listarProdutos();
-                            $a = 0;
-                            if ($listaProdutos != null) {
-                                foreach ($listaProdutos as $lp) {
-                                    $a++;
-                                    ?>
-                                    <tr>
-                                        <td><?php print_r($lp->getIdProduto()); ?></td>
-                                        <td><?php print_r($lp->getNomeProduto()); ?></td>
-                                        <td><?php print_r($lp->getVlrCompra()); ?></td>
-                                        <td><?php print_r($lp->getVlrVenda()); ?></td>
-                                        <td><?php print_r($lp->getQtdEstoque()); ?></td>
-                                        <td><a href="cadastroProduto.php?id=<?php echo $lp->getIdProduto(); ?>"
-                                                class="btn btn-light">
-                                                <img src="img/edita.png" width="32"></a>
-                                            </form>
-                                            <button type="button" 
-                                                    class="btn btn-light" data-bs-toggle="modal" 
-                                                    data-bs-target="#exampleModal<?php echo $a; ?>">
-                                                <img src="img/delete.png" width="32"></button></td>
-                                    </tr>
-                                    <!-- Modal -->
-                                <div class="modal fade" id="exampleModal<?php echo $a; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                                <button type="button" class="btn-close" 
-                                                        data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form method="get" action="excluiProduto.php">
-                                                    <label><strong>Deseja excluir o produto 
-                                                            <?php echo $lp->getNomeProduto(); ?>?</strong></label>
-                                                    <input type="hidden" name="ide" 
-                                                           value="<?php echo $lp->getIdProduto(); ?>">
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="submit" class="btn btn-primary">Sim</button>
-                                                <button type="reset" class="btn btn-secondary" 
-                                                        data-bs-dismiss="modal">Não</button>
-                                            </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
+                        <table class="table table-striped table-responsive"
+                               style="border-radius: 3px; overflow:hidden;">
+                            <thead class="table-dark">
+                                <tr><th>Código</th>
+                                    <th>Nome</th>
+                                    <th>Compra (R$)</th>
+                                    <th>Venda (R$)</th>
+                                    <th>Estoque</th>
+                                    <th>Ações</th></tr>
+                            </thead>
+                            <tbody>
                                 <?php
-                            }
-                        }
-                        ?>
-                        </tbody>
-                    </table>
+                                $pcTable = new ProdutoController();
+                                $listaProdutos = $pcTable->listarProdutos();
+                                $a = 0;
+                                if ($listaProdutos != null) {
+                                    foreach ($listaProdutos as $lp) {
+                                        $a++;
+                                        ?>
+                                        <tr>
+                                            <td><?php print_r($lp->getIdProduto()); ?></td>
+                                            <td><?php print_r($lp->getNomeProduto()); ?></td>
+                                            <td><?php print_r($lp->getVlrCompra()); ?></td>
+                                            <td><?php print_r($lp->getVlrVenda()); ?></td>
+                                            <td><?php print_r($lp->getQtdEstoque()); ?></td>
+                                            <td><a href="cadastroProduto.php?id=<?php echo $lp->getIdProduto(); ?>"
+                                                   class="btn btn-light">
+                                                    <img src="img/edita.png" width="32"></a>
+                                                </form>
+                                                <button type="button" 
+                                                        class="btn btn-light" data-bs-toggle="modal" 
+                                                        data-bs-target="#exampleModal<?php echo $a; ?>">
+                                                    <img src="img/delete.png" width="32"></button></td>
+                                        </tr>
+                                        <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal<?php echo $a; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                    <button type="button" class="btn-close" 
+                                                            data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form method="get" action="excluiProduto.php">
+                                                        <label><strong>Deseja excluir o produto 
+                                                                <?php echo $lp->getNomeProduto(); ?>?</strong></label>
+                                                        <input type="hidden" name="ide" 
+                                                               value="<?php echo $lp->getIdProduto(); ?>">
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="submit" class="btn btn-primary">Sim</button>
+                                                            <button type="reset" class="btn btn-secondary" 
+                                                                    data-bs-dismiss="modal">Não</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                                </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
